d50eccc22ee818995ccc7cf9b72a0849b1fa9aed
shota-nagato <nagato@hab-co.jp>
2023-09-23 17:08:46 +0900

    ヘッダーコンポーネント作成

---
+++ src/components/Home/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="navbar bg-base-100 shadow px-4">
+      <div class="flex-1 font-bold">
+        <router-link to="/">
+          Timer App
+        </router-link>
+      </div>
+      <div class="flex-none">
+        <button class="btn btn-outline btn-primary mr-4">
+          <router-link to="/signup">
+            会員登録
+          </router-link>
+        </button>
+        <button class="btn btn-ghost">
+          <router-link to="/login">
+            ログイン
+          </router-link>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
