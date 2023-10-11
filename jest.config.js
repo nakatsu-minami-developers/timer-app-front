a313373c9ea6e1b36ace2a6bf5855c48df3db8e5
shota-nagato <shota.nagato@gmail.com>
2023-10-11 21:45:44 +0900

    vueテスト設定

--- jest.config.js
+++ jest.config.js
@@ -20,10 +20,14 @@ module.exports = {
     "**/?(*.)+(spec|test).+(ts|tsx|js)"
   ],
   "transform": {
+    "vee-validate/dist/rules": "babel-jest",
     ".*\\.(vue)$": "@vue/vue3-jest",
     "^.+\\.(ts|tsx)$": "babel-jest"
   },
   "moduleNameMapper": {
     "^@/(.*)$": "<rootDir>/src/$1"
-  }
+  },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!vee-validate/dist/rules)',
+  ]
 }
